008b392b4c22b8869068e1931705c638d0695644
16Yangseungho <zlqhem4656@gmail.com>
2021-09-17 19:43:06 +0900

    const foodLike
    const foodLike, name,image추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+const foodLike = [
+  {
+    name: "chiken", 
+    image: "https://health.chosun.com/site/data/img_dir/2021/03/31/2021033102448_0.jpg"
+  }, 
+  {
+    name: "ham", 
+    image: "https://www.the-pr.co.kr/news/photo/202009/45637_69367_331.jpg"
+  } 
+]
 function App() {
   return (
     <div>
