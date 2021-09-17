04fc60ebd2f45d24ed38afccdb16b9b61247534f
16Yangseungho <zlqhem4656@gmail.com>
2021-09-17 16:35:38 +0900

    props
    props에서 kimchi빼고 전부 삭제

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Food fav="kimchi" someting = {true} papapa = {['hello', 1,2, true]}/>
+      <Food fav="kimchi" />
     </div>
   )
 }
 
-function Food(props){
-  console.log(props);
+function Food(foo){
+  console.log(foo);
   return <h1>I like potaoa</h1>
 }
 export default App;
