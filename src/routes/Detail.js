1ac4c2458d83ee3be7909717bf987626b01e0f21
16Yangseungho <zlqhem4656@gmail.com>
2021-11-03 16:38:15 +0900

    Movie.js
    Movie.js에 링크추가, Detail.js 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,18 @@
-function Detail(props){
-    console.log(props)
+import React from "react"
+class Detail extends React.Component{
+    componentDidMount(){
+    const { location,history} =this.props
+    if (location.state === undefined){
+        history.push('/')
+    }
+}
+    render () {
+    
     return(
         <span>Hello</span>
     )
 }
+}
+
     
 export default Detail
