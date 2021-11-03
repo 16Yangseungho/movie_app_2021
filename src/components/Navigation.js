e6ec7c12393602f7f4975fb01f3f7fe6268e29f3
16Yangseungho <zlqhem4656@gmail.com>
2021-11-03 16:01:22 +0900

    Detail
    Detail.js생성후 app.js 연결

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,11 @@
 import {Link} from 'react-router-dom'
+import './Navigation.css'
 
-function Navigation() {
+function Navigation() {    
     return(
-        <div>
+        <div className='nav'>
         <Link to='/'>Home</Link>
-        <Link to ='/about'>about</Link>
+        <Link to ={{pathname: '/about', state: {fromNavigation: true}}}>about</Link>
         </div>
     )
 }
---
+++ src/routes/Detail.js
@@ -0,0 +1,8 @@
+function Detail(props){
+    console.log(props)
+    return(
+        <span>Hello</span>
+    )
+}
+    
+export default Detail
